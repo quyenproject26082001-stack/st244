a358a4d0295571b3e31fac3ed703eb450c4fbc07
unknown <quyenle26082001@gmail.com>
2026-04-03 16:06:36 +0700

    done

--- run_build2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Android\Android Studio\jbr
-set PATH=%JAVA_HOME%\bin;%PATH%
-cd /d "C:\APPS\st244\ST244_Call The Cat Oia Oia Clicker"
-gradlew.bat assembleDebug > build_log.txt 2>&1
-echo BUILD_DONE exit=%ERRORLEVEL% >> build_log.txt
